c7ddffd5257db33b73795f461674c17192e6d3bf
tanglid <tanglid@chanjet.com>
2018-12-01 21:14:31 +0800

    webapp

---
+++ src/rest/json_encoder.py
@@ -0,0 +1,7 @@
+class JSONEncoder(json.JSONEncoder):
+	def default(self, o):
+		if isinstance(o,ObjectId):
+			return str(o)
+		if isinstance(o,Decimal):
+			return str(o)
+		return json.JSONEncoder.default(self,o)
