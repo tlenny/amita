04481dbfacc84b77475aa9588e6bffe90e91a589
tanglid <tanglid@chanjet.com>
2019-01-22 08:50:39 +0800

    fix bug

--- src/task/pl_job.py
+++ src/task/pl_job.py
@@ -6,17 +6,26 @@ Created on 2019年1月16日
 from task.base_job import BaseJob
 from portfolio.bias_kdj import BiasKdjAnls
 
+anls_factory = [BiasKdjAnls()]
+
 class PlJob(BaseJob):
     '''
     classdocs
     '''
 
 
-    def __init__(self, params):
+    def __init__(self):
         '''
         Constructor
         '''
     
     def run(self,day):
-        BiasKdjAnls().anls(day)
-        
+        for i in range(len(anls_factory)):
+            a = anls_factory[i]
+            print('分析【%s】'%(a.type(),))
+            a.anls(day)
+            print('分析【%s】结束'%(a.type(),))
+
+if __name__ == '__main__':
+    PlJob().run('2019-01-21')
+    pass
